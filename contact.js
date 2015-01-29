aacb210ae04439e7ebf27196ae3fe5b2f94428cf
Tony Gaskell <thgaskell@gmail.com>
2015-01-28 21:14:48 -1000

    Add contact info tests

---
+++ contact.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  /**
+   * Returns all the emails in a phrase.
+   * @param  {String} phrase
+   * @return {[String]} an array of emails, or an empty array if none are found.
+   */
+  findEmails: function(phrase) {
+  },
+
+  /**
+   * Finds all the phone numbers in a phrase
+   * @param  {String} phrase
+   * @return {[String]]} an array of phone numbers.
+   */
+  findPhoneNumbers: function(phrase) {
+  }
+
+};
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var cloudExercise = require('../cloud');
+var emailExercise = require('../contact');
 var chai = require('chai');
     chai.should();
 
@@ -42,5 +43,45 @@ describe('the cloud', function() {
   // it('can replace any string with another string (maintains capitalization)', function() {
   //   var result = cloudExercise.replaceWords('Developers developers Developers', 'developers', 'designers');
   //   result.should.equal('Designers designers Designers');
+  // });
+});
+
+describe('emails', function() {
+  it('fetches an email', function() {
+    var result = contactExercise.findEmails('My email is test@test.com');
+    result.should.equal(["test@test.com"]);
+  });
+
+  it('fetches all emails', function() {
+    var result = contactExercise.findEmails('Please contact us at test@test.com, test1@test.com');
+    result.should.equal(['test@test.com', 'test1@test.com']);
+  });
+
+  it('does not match using @ as shorthand', function() {
+    var result = contactExercise.findEmails('Meet me @ Waikiki');
+    result.should.equal([]);
+  });
+
+  it('does not fetch Twitter handles', function() {
+    var result = contactExercise.findEmails('Follow @brendaneich on Twitter');
+    result.should.equal([]);
+  });
+});
+
+describe('phone numbers', function() {
+  it('fetches a simple phone number', function() {
+    var result = contactExercise.findPhoneNumbers('My number is 8085551234');
+    result.should.equal(['8085551234']);
+  });
+
+  it('fetches a phone number with dashes', function() {
+    var result = contactExercise.findPhoneNumbers('My number is 808-555-1234');
+    result.should.equal(['808-555-1234']);
+  });
+
+  // CHALLENGE
+  // it('fetches a phone number with the area code in parens', function() {
+  //   var result = regexExercise.findPhoneNumbers('My number is (808) 555-1234');
+  //   result.should.equal(['(808) 555-1234'])
   // });
 });
